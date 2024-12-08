24a978494aff7450e1951aa0cbbe783abc822947
chinmay padhi <padhichinmay108@gmail.com>
2024-12-08 12:31:52 +0530

    first commit

---
+++ Reverse a string without using reverse function/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Reverse_a_string_without_using_reverse_function_
+{
+    class Program
+    {
+
+         static void StrReverse(string str)
+        {
+            char[] arr = str.ToCharArray();
+            String str1 = "";
+
+            for(int i=arr.Length-1;i>=0;i--)
+            {
+                str1 += arr[i];
+            }
+            Console.WriteLine(str);
+            Console.WriteLine(str1);
+        }
+        static void Main(string[] args)
+        {
+            StrReverse("Chinmay");
+            Console.ReadLine();
+        }
+    }
+}
